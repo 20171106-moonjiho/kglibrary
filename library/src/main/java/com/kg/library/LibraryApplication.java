6549110575f4e9bf26c5b6653776eddbaf5f557a
User <User@DESKTOP-LS9HN0Q>
2023-12-28 10:01:00 +0900

    실행 확인

--- library/src/main/java/com/kg/library/LibraryApplication.java
+++ library/src/main/java/com/kg/library/LibraryApplication.java
@@ -2,8 +2,10 @@ package com.kg.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+
 public class LibraryApplication {
 
 	public static void main(String[] args) {
