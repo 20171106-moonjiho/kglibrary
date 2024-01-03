8536014572ad39d8fc4df9bd37ab4ae4ec7490ca
user1 <user1@DESKTOP-KHOHI28>
2024-01-03 09:23:58 +0900

    커밋

--- library/src/main/java/com/kg/library/notice/NoticeBoardController.java
+++ library/src/main/java/com/kg/library/notice/NoticeBoardController.java
@@ -4,12 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class NoticeBoardController {
 	@Autowired NoticeBoardService service;
+	@Autowired
+	private HttpSession session;
 	
+	//공지사항 메인
 	@RequestMapping("NoticeBoard")
 	public String NoticeBoard() {
 		return "notice/noticeboard";
 	}
+	
+	//공지사항 글쓰기
+	
+	//공지사항 내용
+	
+	//공지사항 삭제
+	
+	//공지사항 수정
+	
 }
